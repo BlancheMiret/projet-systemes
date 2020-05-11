e90d3d573f397a68167801822d03379c70df563a
chekrouh <chekrouh@informatique.univ-paris-diderot.fr>
2020-05-11 15:34:56 +0200

    add paths_manager.c

---
+++ src/paths_manager.c
@@ -0,0 +1,92 @@
+ 
+#include "paths_manager.h"
+
+
+ //fonction pour vérifier si le chemin existe déjà
+//renvoie 1 si le chemin existe sinon 0
+int find_path(char * file_paths, char * file_to_tag ){
+
+  char *path = realpath(file_to_tag, NULL);
+  char *line_buf = NULL;
+  size_t line_buf_size = 0;
+  ssize_t line_size;
+  int line_count = 0;
+  FILE *file = fopen(file_paths, "r");
+
+    //retourne le nombre de caractères de la première ligne
+  line_size = getline(&line_buf, &line_buf_size, file);
+  
+  while (line_size>= 0) {
+
+    printf("LINE %s",line_buf );
+    
+    if (strncmp(path, line_buf,strlen(line_buf)-1) == 0) {
+     line_count++;
+   }
+
+        //ligne suivante:
+   line_size = getline(&line_buf, &line_buf_size, file);
+ }
+
+    //printf("nb of lines equal to path = %d\n",line_count);
+
+ if(line_count == 0) return 0;
+
+ return 1;
+}
+
+
+//Fonction qui ajoute le chemin du fichier taggé dans un fichier .txt
+int add_path(char * filename){
+
+    //On récupère le chemin d'accès absolu du fichier
+  char *path = realpath(filename, NULL);
+
+  if(path == NULL){
+   printf("cannot find file with name[%s]\n", filename);
+ } 
+ else{
+   printf("path --> %s \n", path);
+         //free(path);
+ }
+
+ FILE  *file;
+
+    //fichier où on va stocker les chemins des fichiers taggés
+ const char *filename_paths = "paths.txt";
+ file = fopen(filename_paths, "a");
+
+ if (file == NULL)
+ {
+  fprintf(stderr, "cannot open %s for appending\n", filename_paths);
+  return -1;
+}
+
+    //fseek: positionne le "curseur de fichier" à une distance comptée en octets à partir du début, de la fin ou de la position courante 
+    //on positionne ici à la fin du fichier
+fseek (file, 0, SEEK_END);
+
+    //ftell renvoie la position courante du « curseur de fichier », 
+int size = ftell(file);
+
+if (size == 0) {
+  printf("file is empty !\n");
+  fprintf(file, "%s", path);
+}
+else {
+
+  if(find_path("paths.txt", filename) == 1){
+
+   printf("Le chemin existe déjà!\n");
+ }
+
+ else {
+  fprintf(file, "\n%s", path);
+ }
+
+}
+fclose(file);
+
+
+
+}
