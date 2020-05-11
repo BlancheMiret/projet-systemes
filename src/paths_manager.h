e13eb24c295e214372015a10a2458ac73fdcd2c3
chekrouh <chekrouh@informatique.univ-paris-diderot.fr>
2020-05-11 15:33:58 +0200

    add paths_manager.h

---
+++ src/paths_manager.h
@@ -0,0 +1,10 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/xattr.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
