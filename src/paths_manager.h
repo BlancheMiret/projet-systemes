38a19783796b429141e22e5f1d1a803ae94b9207
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-05-12 21:09:14 +0200

    update paths_manager.h

--- src/paths_manager.h
+++ src/paths_manager.h
@@ -8,7 +8,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
+int check_file_existence(char * filename);
 char * absolute_path(char * filename);
 int find_path(char * file_paths, char * file_to_tag );
 int add_path(char * filename);
