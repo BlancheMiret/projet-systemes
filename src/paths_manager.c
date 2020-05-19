f46247a4dc6cfa8c50213ea1b1a234a050bb3f40
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-05-19 12:19:41 +0200

    correction dans paths_manager.c

--- src/paths_manager.c
+++ src/paths_manager.c
@@ -1,13 +1,4 @@
-#include <malloc.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/xattr.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
+#include "paths_manager.c"
 
 
 
