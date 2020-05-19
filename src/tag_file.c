5b5609490f458ec064d70fd7c63af5422e61c66c
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-05-19 02:47:17 +0200

    update unlink_tag

--- src/tag_file.c
+++ src/tag_file.c
@@ -65,8 +65,7 @@ int set_tag(char * path, char * usertag, char * all_tags, int flags){
     if(flags){
 
     	if(setxattr(path,usertag,all_tags,strlen(all_tags),XATTR_REPLACE) > -1){
-
-    		printf("fichier taggé! \n");			
+			
     		add_path(path);
 
     	}   
@@ -254,249 +253,195 @@ int unlink_tag(char * filename, char * tags[], size_t tags_size){
 
 	int val;
 	char reply[100];
+	char * subtags[100];
+	char subtags_concat[1024];
 	char *path = realpath(filename, NULL);
-	char * usertag = "user.tag";
+	char * usertag = "user.tags";
 	char buff_tag[1024];
 	struct tag_node * children_list=NULL;
 	char *all_tags =malloc(1024*sizeof(char));
 	memset(all_tags,'\0',1024);
 	char *ptr = NULL;
 	int count=0;
+	int j = 0;
 
-/**
-struct tag_node *tag =(struct tag_node *) malloc(sizeof(struct tag_node));
-memcpy(tag->name, "jaune", strlen("jaune"));
-tag->next = (struct tag_node *) malloc(sizeof(struct tag_node));
-memcpy(tag->next->name, "rouge", strlen("rouge"));
-tag->next->next = (struct tag_node *) malloc(sizeof(struct tag_node));
-memcpy(tag->next->next->name, "bleu", strlen("bleu"));
-tag->next->next->next = NULL; **/
+    
+	val = listxattr(path, NULL, 0);
 
+	if (val == -1) {
+		perror("listxattr error");
+		return 0;
+	}
+
+	//Cas où user.tags n'existe pas
+	if (val == 0) {
+
+		printf("Le fichier ne contient aucun tag!\n");
+		return 0;
+
+	}
 
 	for (int i=0; i<tags_size;i++){
 
-		children_list=get_tag_children(tags[i]);
-//children_list=tag;
-		val = getxattr(filename,usertag, &buff_tag, sizeof(buff_tag)) ;
+		val = getxattr(filename,usertag, &buff_tag, sizeof(buff_tag));
 		
-		buff_tag[val] = '\0';
-		printf("FIRST buff_tag %s\n ", buff_tag);
+		if(val == -1 ){
+			perror("getxattr error: ");
+			return 0;
+		} 
 
-/********** OK ***********/
-		if(val > 0){
-
-//int init_size = strlen(buff_tag);
-			char delim[] = "/";
-
-			ptr = strtok(buff_tag, delim);
-
-			while(ptr != NULL)
-			{
-				printf("'%s'\n", ptr);
-
-				if(strcmp(ptr,tags[i]) != 0){
-
-					strcat(all_tags,ptr); 
-					strcat(all_tags,"/"); 
-
-				}
-
-				ptr = strtok(NULL, delim);
-			}
-
-//all_tags = contient tous les tags sauf tags[i]
-			strcat(all_tags,"\0");
-//printf("all_tags  %s\n",all_tags);
-
-//On attribut les sous-tags sans tags[i]
-			if(setxattr(path,usertag,all_tags,strlen(all_tags),XATTR_REPLACE) > -1){
-				printf("all_tags %s ", all_tags);
-				printf("tag set!\n");
-				add_path(filename);
-//return 0;
-
-			}   
-
-			else {
-				perror("error set: ");
-				return 1;
-			}
-
-
+		//Cas où user.tags existe mais ne contient aucun tag
+		if(val == 0){
+			printf("Le fichier ne contient aucun tag!\n");
+			return 0;
 		}
 
-		memset(all_tags,'\0',1024);
-//printf("all_tags %s ", all_tags);
+		//Suppression de tags[i] 
 
-//Si le tag a des enfants dans la hierarchie
+		char delim[] = "/";
+
+		ptr = strtok(buff_tag, delim);
+
+		while(ptr != NULL)
+		{
+			printf("'%s'\n", ptr);
+
+			if(strcmp(ptr,tags[i]) != 0){
+
+				strcat(all_tags,ptr); 
+				strcat(all_tags,"/"); 
+
+			}
+
+			ptr = strtok(NULL, delim);
+		}
+
+		//all_tags = contient tous les tags sauf tags[i]
+		strcat(all_tags,"\0");
+		//printf("all_tags  %s\n",all_tags);
+
+		//On attribut les sous-tags sans tags[i]
+		val = set_tag(path, usertag, all_tags,1);
+		if(val == 0) return 0;	
+
+	    children_list=get_tag_children(tags[i]);
+		
+        
+		buff_tag[val] = '\0';
+		//printf("VAL  %d\n ",(int) val);
+		//printf("FIRST buff_tag %s\n ", buff_tag);
+
+        //Si le tag a des enfants dans la hierarchie
 		if(children_list != NULL){
+   
+            //On vérifie si un ou plusieurs tags de children_list sont liés au fichier
+            //On ajoute les tags de children_list qui sont liés dans subtags
+			while(children_list != NULL) {
 
-			printf("Le fichier concerné possède également le ou les sous-tags suivant hériant de %s :\n", tags[i]);
+				if(check_tag_existence(buff_tag,  children_list->name) == 1){
+				
+					subtags[j] = children_list->name;
+					j++;
+				
+				}
+				
+				children_list = children_list->next;
+			}
 
-			print_list(children_list);
+			if(j == 0){
 
-			printf("souhaitez vous supprimer également le ou les sous-tags? Répondez par 'oui' ou 'non'\n");
-			scanf("%s", reply);
+				memset(buff_tag,'\0',1024);
+				val = getxattr(filename,usertag, &buff_tag, sizeof(buff_tag));
+	
+				if(val == 0){
+					printf("Le fichier ne contient plus de tags!, il va être supprimé de paths.txt\n");
+					delete_path(filename);
+				}
+				return 1;
 
-			if(strcmp(reply,"oui")==0){
+			} 
+
+			if(j == 1){ 
+				
+				printf("Le fichier concerné possède également le sous-tag suivant hériant de %s :\n", tags[i]);
+				printf("- %s\n", subtags[0]);
+				printf("Souhaitez vous supprimer également ce sous-tag? Répondez par 'oui' ou 'non'\n");
+			}
+
+			if(j > 1){
+
+				printf("Le fichier concerné possède également les sous-tags suivant hériant de %s :\n", tags[i]);
+				for(int i=0; i < j; i++) printf("- %s\n", subtags[i]);
+				printf("Souhaitez vous supprimer également ces sous-tags? Répondez par 'oui' ou 'non'\n");
+			}
+			
+			//print_list(children_list);
+			while(1){
+				scanf("%s", reply);
+				if(strcmp(reply,"oui") == 0 || strcmp(reply,"non") ==0) break;
+				printf("Répondez par 'oui' ou 'non'\n");
+			}
+			
+
+			if(strcmp(reply,"oui") == 0){
 
 				printf("vous avez répondu oui\n");
+				//printf("buff_tag  %s\n", buff_tag);
 
-				val = getxattr(filename,usertag, &buff_tag, sizeof(buff_tag)) ;
-				buff_tag[val] = '\0';
-				printf("buff_tag  %s\n", buff_tag);
+				concatenate_tags(subtags_concat, subtags, j);
 
-				if(val > 0){
+				char delim[] = "/";
 
-					char delim[] = "/";
+				char *ptr = strtok(buff_tag, delim);
+				while(ptr != NULL)
+				{
+					//printf("'%s'\n", ptr);
 
-					char *ptr = strtok(buff_tag, delim);
+					if(strcmp(ptr,tags[i]) != 0 && check_tag_existence(subtags_concat, ptr) == 0){
 
-					while(ptr != NULL)
-					{
-//printf("'%s'\n", ptr);
+						strcat(all_tags,ptr); 
+						strcat(all_tags,"/"); 
 
-						while(children_list != NULL) {
-							printf("ptr  %s\n", ptr);
-//printf("children list  %s\n", children_list->name);
-
-//On vérifie si le tag contenu dans ptr n'est pas le même que les sous-tags dans children_list
-//Si count =
-							if(strcmp(ptr,children_list->name) != 0){
-								count++;
-// printf("ON EST DANS STRCMP\n");
-//count ++ puis on compare count avec size de children_list, si ils sont égaux
-//alors ça veut dire qu'on peut laisser ce tag lié au fichier
-
-//break; 
-
-							}
-
-							children_list = children_list->next;
-						}
-						printf("count   %d\n", count);
-						printf("list_length(tag)    %d\n", list_length(tag) );
-
-//Si count = list_length(tag), alors on peut laissé le tag contenu dans ptr
-//car il est différent de tous les tags contenu dans children_list
-						if(count == list_length(tag) && strcmp(ptr,tags[i]) != 0){
-// printf("count   %d", count);
-							strcat(all_tags,ptr); 
-							strcat(all_tags,"/");
-
-						}
-						count =0;
-						children_list = tag;  
-						ptr = strtok(NULL, delim);
 					}
 
-//all_tags = contient tous les tags sauf ceux qu'on a supprimé
-					strcat(all_tags,"\0");
-//printf("all_tags  %s\n",all_tags);
-
-//On attribut les tags après supression au fichier
-					if(setxattr(path,usertag,all_tags,strlen(all_tags),XATTR_REPLACE) > -1){
-
-						printf("Le tag %s ainsi que ses enfants ont été supprimé !\n", tags[i]);
-						add_path(filename);
-
-					}   
-
-					else {
-						perror("error set: ");
-						return 1;
-					}
-
-
+					ptr = strtok(NULL, delim);
 				}
 
+				//all_tags = contient tous les tags sauf ceux qu'on a supprimé
+				strcat(all_tags,"\0");
+				//printf("all_tags  %s\n",all_tags);
+
+				//On attribut les tags après supression au fichier
+				val = set_tag(path, usertag, all_tags, 1);
+
+				if(val == 0) return 0;
+				printf("Le tag %s ainsi que ses sous-tags ont été supprimé !\n", tags[i]);
+
+		
 			}
 
 			if(strcmp(reply,"non")==0){
+
 				printf("Seul le tag %s a été supprimé !\n", tags[i]);
-/** 
-if(val > 0){
-
-//int init_size = strlen(buff_tag);
-char delim[] = "/";
-
-ptr = strtok(buff_tag, delim);
-
-while(ptr != NULL)
-{
-printf("'%s'\n", ptr);
-
-if(strcmp(ptr,tags[i]) != 0){
-
-strcat(all_tags,ptr); 
-strcat(all_tags,"/"); 
-
-}
 
 
-ptr = strtok(NULL, delim);
-}
-
-//all_subtags = contient tous les sous-tags sauf tagname
-strcat(all_tags,"\0");
-//printf("all_subtags  %s\n",all_subtags);
-
-//On attribut les sous-tags sans tagname
-if(setxattr(path,usertag,all_tags,strlen(all_tags),XATTR_REPLACE) > -1){
-
-printf("tag set!\n");
-add_path(filename);
-return 0;
-
-}   
-
-else {
-perror("error set: ");
-return 1;
-}
-
-
-}
-
-**/
 			}
-
-
-
-
 		}
 
-//Ajouter le cas: si on supprime le dernier tag on supprime le chemin dans paths.txt 
-//Ajouter une fonction delete_path()
 
-
-
-
-		memset(buff_tag,'\0',1024);
 	}
 
 	memset(buff_tag,'\0',1024);
-	val = getxattr(filename,usertag, &buff_tag, sizeof(buff_tag)) ;
-	buff_tag[val] = '\0';
-	if (val == -1) perror("getxattr error: ");
 
-	if (val == 0){
+    
+	val = getxattr(filename,usertag, &buff_tag, sizeof(buff_tag));
+		if(val == 0){
+			printf("Le fichier ne contient plus de tags!, il va être supprimé de paths.txt\n");
+			delete_path(filename);
 
-		printf("Il n'y a aucun dans ce fichier\n");
-		delete_path(filename);
-	}
+		}
 
-
-
+	return 1;
 
 }
-
-
-
-
-
-
-
-
-
 
