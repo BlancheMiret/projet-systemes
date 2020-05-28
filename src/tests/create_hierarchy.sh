72a39a02b1963bc0962a69e9da267be4c28d1917
MIRET blanche <blanchemiret@gmail.com>
2020-05-28 18:17:33 +0200

    Create test scripts

---
+++ src/tests/create_hierarchy.sh
@@ -0,0 +1,8 @@
+tag create -n couleur utilisateur film livre genre
+tag create couleur rouge vert bleu rose jaune
+tag create bleu bleu-clair azur turquoise
+tag create -n musique
+tag create genre western horreur drame
+tag create jaune ocre
+echo "Hierarchy created :"
+tag print
