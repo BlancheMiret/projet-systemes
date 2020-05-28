1cd4c85c307c0fddd3426613bb8a2b460b877217
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-05-28 22:11:07 +0200

    update tag_file.h

--- src/tag_file.h
+++ src/tag_file.h
@@ -5,6 +5,7 @@
 int link_tag(char *filename, char * tags[], size_t tags_size);
 int unlink_tag(char * filename, char * tags[], size_t tags_size, int ask);
 void * get_file_tag_list(char * path);
+int delete_all_tags(char * filename);
 int for_all_files_delete(char * tag[]);
 int reset_all_files();
 #endif
