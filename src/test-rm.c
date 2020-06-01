98736a2b387099aa4b4fa290d915b0fcc1abb2a2
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-06-01 18:56:15 +0200

    supp test-rm.c

---
+++ src/test-rm.c
@@ -0,0 +1,60 @@
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <sys/types.h>
+#include  <unistd.h>
+#include  <sys/wait.h> 
+#include "paths_manager.c"
+
+int main(int argc, char *argv[])
+{
+  
+    init_file_paths();
+
+    pid_t pid;
+
+    /* fork a child process */ 
+    pid = fork();
+    if (pid < 0)
+    { 
+        /* error occurred */ 
+        fprintf(stderr, "Fork Failed"); 
+        return 1; 
+    } 
+    else if (pid == 0) 
+    { 
+   
+        /* child process */ 
+      if(strcmp(argv[1],"-i") == 0){
+        argv[0] = "rm";
+
+        execvp("rm", argv);
+
+      }
+     
+    } 
+
+    else 
+    {   
+      
+         /* parent process */ 
+        /* parent will wait for the child to complete */ 
+    
+    char *arg = NULL;
+    for(int i=2; i< argc-2 ; i++){
+
+    arg = realpath(argv[i],NULL);
+    if (arg == NULL) exit(EXIT_FAILURE);
+  
+    if (find_path(arg) == 1) {
+
+      delete_path(arg);  
+
+    }
+   
+    
+    wait(NULL); 
+
+    } 
+
+    return 0;
+}
