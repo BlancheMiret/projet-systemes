101ad61c5e067e3dffa2e29eb0f8dea574eea696
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-06-06 17:40:10 +0200

    ajout d'un test tag_files2.sh

---
+++ src/tests/tag_files2.sh
@@ -0,0 +1,20 @@
+touch test1
+touch test2
+touch test3
+touch test4
+touch test5
+tag link test1 thriller roman ballade
+tag link test2 film science-fiction "space opera"
+tag link test3 historique
+tag link test4 autofiction 
+tag link test5 thriller psychologique historique
+echo "test1 tags :"
+tag print test1
+echo "test2 tags :"
+tag print test2
+echo "test3 tags :"
+tag print test3
+echo "test4 tags :"
+tag print test4
+echo "test5 tags :"
+tag print test5
