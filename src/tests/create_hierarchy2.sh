7b39c768f0782a86c9acb959b237bbdcfc9171d2
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-06-06 17:30:12 +0200

    ajout d'un test create_hierarchy2.sh

---
+++ src/tests/create_hierarchy2.sh
@@ -0,0 +1,10 @@
+tag create couleur film livre musique
+tag create -f musique jazz ballade rock 
+tag create -f livre roman BD manga essai dictionnaire scolaire biographie
+tag create -f biographie autobiographie autofiction 
+tag create -f roman policier historique science-fiction
+tag create -f science-fiction "space opera"
+tag create -f policier thriller
+tag create -f thriller psychologique
+echo "Hierarchy created :"
+tag print
