cfd610be90f605b71c163b08a256013713d49ce0
MIRET blanche <blanchemiret@gmail.com>
2020-06-10 19:22:23 +0200

    Update test1

--- src/tests/create_hierarchy.sh
+++
@@ -1,8 +0,0 @@
-tag create utilisateur film livre genre couleur
-tag create -f couleur rouge vert bleu rose jaune
-tag create -f bleu bleu-clair azur turquoise
-tag create musique
-tag create -f genre western horreur drame
-tag create -f jaune ocre
-echo "Hierarchy created :"
-tag print
--- src/tests/research.sh
+++
@@ -1,6 +0,0 @@
-echo "Files with color"
-tag search couleur
-echo "Files with color but not rose"
-tag search couleur -not rose
-echo "Files with bleu but not turquoise and not jaune"
-tag search bleu -not turquoise -not jaune
