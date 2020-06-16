7388290ac648677acf09d6be20bcb8ebf24b6ffd
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-06-17 00:30:40 +0200

    Update test_all_functions.sh

--- tests/test_all_functions.sh
+++ tests/test_all_functions.sh
@@ -1,5 +1,7 @@
+echo "TEST ALL TAG COMMANDS"
+echo
 # Create a hierarchy
-echo "creating hierarchy..."
+echo "Creating hierarchy..."
 tag create couleur film livre musique
 tag create -f musique jazz ballade rock 
 tag create -f livre roman BD manga essai dictionnaire scolaire biographie
@@ -8,43 +10,73 @@ tag create -f roman policier historique science-fiction
 tag create -f science-fiction "space opera"
 tag create -f policier thriller
 tag create -f thriller psychologique
-echo "Hierarchy created :"
+echo " -------- Hierarchy print -------- "
 tag print
-# Tagging files
 touch test1
 touch test2
 touch test3
 touch test4
 touch test5
-echo "tagging files..."
+touch test6
+echo
+echo " -------- Tagging files -------- "
 tag link test1 thriller roman policier ballade
 tag link test2 film science-fiction "space opera"
 tag link test3 historique jazz
 tag link test4 autofiction roman
 tag link test5 film thriller psychologique
-echo "test1 tags :"
+tag link test6 musique jazz film
+echo 
+echo " -------- Printing tags of each file -------- "
+echo "-- tag print test1"
 tag print test1
-echo "test2 tags :"
+echo
+echo "-- tag print test2"
 tag print test2
-echo "test3 tags :"
+echo
+echo "-- tag print test3"
 tag print test3
-echo "test4 tags :"
+echo
+echo "-- tag print test4"
 tag print test4
-echo "test5 tags :"
+echo
+echo "-- tag print test5"
 tag print test5
-# Searching tags
-echo "searching tags..."
-echo "Files with psychologique but not film thriller:"
+echo 
+echo " -------- Deleting tags from files -------- "
+echo "-- tag unlink test4 roman"
+tag unlink test4 roman
+echo
+echo "-- tag unlink test1 ballade thriller"
+tag unlink test1 ballade thriller
+echo
+echo "-- tag unlink test6 --all"
+tag unlink test6 --all
+echo
+echo " -------- Searching tags -------- "
+echo "-- tag search psychologique -not policier -not thriller"
 tag search psychologique -not policier -not thriller
-echo "Files with film thriller:"
+echo
+echo "-- tag search thriller film thriller"
 tag search thriller film thriller
-echo "Files with science-fiction but not film:"
+echo
+echo "-- tag search science-fiction -not film"
 tag search science-fiction -not film
-echo "Files with autofiction but not roman:"
+echo
+echo "-- tag search autofiction -not roman"
 tag search autofiction -not roman
-echo "Files with autofiction:"
+echo
+echo "-- tag search autofiction"
 tag search autofiction
-
+echo
+echo " -------- Deleting tags from hierarchy -------- "
+echo "-- tag delete musique"
+tag delete musique
+echo "-- tag delete policier"
+tag delete policier
+echo 
+echo "-- tag print"
+tag print
 # Reset
 tag reset
 rm test1 test2 test3 test4 test5
